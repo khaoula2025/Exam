07389f088cc0f67277ee204547c4b063e234cb52
khaoula blidi <khaoula@gmail.com>
2025-03-11 22:04:06 +0100

    projet py apres conflit 2

--- projet.py
+++ projet.py
@@ -6,6 +6,16 @@ def somme (T):
     return S
 
 Data = [1,2,3]
+
 Som=sum(Data)
 print('la somme est : ', Som)
+
+
+if Data:
+
+   print('la somme est : ', Sum(Data))
+   print('le min est :',min(Data))
+   print('le max est:', max(Data))
+else:
+   print('dossier vide')
 
